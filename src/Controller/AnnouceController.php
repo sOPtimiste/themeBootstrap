1907c9290b88737e80a6130fc344e6ca811ef2f9
sOPtimiste <mbackesall33@gmail.com>
2021-06-21 10:13:52 +0000

    Handle announcement

--- src/Controller/AnnouceController.php
+++ src/Controller/AnnouceController.php
@@ -2,10 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Announce;
 use App\Repository\AnnounceRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AnnouceController extends AbstractController
 {
@@ -25,12 +36,47 @@ class AnnouceController extends AbstractController
     }
 
     /**
-     * @Route("/announce/create", name="create_annouce")
+     * @Route("/announce/create", name="create_annouce",methods={"GET","POST"})
      */
 
-    public function create(){
+    public function create(Request $request,EntityManagerInterface $em): Response
+    {
+        $announce = new Announce();
+        $form = $this->createFormBuilder($announce)
+                        ->add('title',null) 
+                        ->add('slug',null)
+                        ->add('description',null)
+                        ->add('price',null)
+                        ->add('address',null)
+                        ->add('coverImage',null,['required' => false,])
+                        ->add('rooms',null)
+                        ->add('isAvailable',null)
+                        ->add('createdAT',null)
+                        ->getForm()
 
-        return $this->render('annouce/create.html.twig',[]);
+         ;    
+         $form->handleRequest($request);
+
+         if($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($announce); 
+
+
+            $em->flush();
+
+            return $this->redirectToRoute('app_annouce');
+            
+
+         }
+        
+
+        return $this->render('annouce/create.html.twig',[
+
+            'formulaire' => $form->createView()
+        ]);
+
+
+        
 
     }
 }
